f23b3b19c9aec73b19c3cb6f4e29403c295d9f7f
João Gabriel <jgbrlmotta@gmail.com>
2024-03-25 20:05:12 -0300

    exercicio_ts

---
+++ exercicio_ts/src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+console.log(multiplicacao(2, 2));
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudacao("João"));
---
+++ exercicio_ts/src/exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicacao(numero1:number,numero2: number):number {
+    return numero1 * numero2;
+}
+
+console.log(multiplicacao(2,2));
+
+function saudacao(nome:string):string {
+    return `Olá ${nome}`
+}
+
+console.log(saudacao("João"));
